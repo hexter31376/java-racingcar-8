db2367656aeeb8811e70e36bd94eb480594028bb
teamplay <67694293+hexter31376@users.noreply.github.com>
2025-10-27 04:29:35 +0900

    refactor(Car): 5글자임에도 예외를 던지게 하던 = 제거

--- src/main/java/racingcar/domain/car/Car.java
+++ src/main/java/racingcar/domain/car/Car.java
@@ -19,7 +19,7 @@ public class Car {
         if (name == null || name.isBlank()) {
             throw new IllegalArgumentException("자동차 이름은 비어 있을 수 없습니다. : " + name);
         }
-        if (name.length() >= 5) {
+        if (name.length() > 5) {
             throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다. : " + name);
         }
     }
