fab87297f95cd931e87b16dc1f49e0dbfb81cdb8
teamplay <67694293+hexter31376@users.noreply.github.com>
2025-10-26 23:08:37 +0900

    refactor(Cars): 잘못된 중복 체크 로직 삭제

--- src/main/java/racingcar/domain/car/Cars.java
+++ src/main/java/racingcar/domain/car/Cars.java
@@ -4,36 +4,30 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Cars {
-    private final Map<String, Car> values;
+    private final Map<String, Car> cars;
 
-    private Cars(Map<String, Car> value) {
-        validate(value);
-        this.values = value;
+    private Cars(Map<String, Car> cars) {
+        this.cars = cars;
     }
 
     public static Cars validatedOf(Map<String, Car> cars) {
+        validate(cars);
         return new Cars(cars);
     }
 
-    public void validate(Map<String, Car> value) {
-        if (values == null || values.isEmpty()) {
+
+    public static void validate(Map<String, Car> cars) {
+        if (cars == null || cars.isEmpty()) {
             throw new IllegalArgumentException("자동차는 한 대 이상 있어야 합니다.");
         }
     }
 
     public Map<String, Car> getValues() {
-        return new HashMap<>(values);
+        return cars;
     }
 
     public void moveCar(String name) {
-        Car car = values.get(name);
-        validate(car.getName());
+        Car car = cars.get(name);
         car.move();
-    }
-
-    public void validate(String name) {
-        if (values.containsKey(name)) {
-            throw new IllegalArgumentException("자동차 이름은 중복될 수 없습니다. : " + name);
-        }
     }
 }
