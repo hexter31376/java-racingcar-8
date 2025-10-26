2a195e411e16250c239ab9f7f41f77272d44f44d
teamplay <67694293+hexter31376@users.noreply.github.com>
2025-10-27 03:03:20 +0900

    refactor(InputTemplate): 앙증맞은 글자 템플릿 맞추기

--- src/main/java/racingcar/view/input/enums/InputTemplate.java
+++ src/main/java/racingcar/view/input/enums/InputTemplate.java
@@ -2,7 +2,7 @@ package racingcar.view.input.enums;
 
 public enum InputTemplate {
     INPUT_CAR_NAMES_MESSAGE("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
-    INPUT_TRY_COUNT_MESSAGE("시도할 회수는 몇 회인가요?");
+    INPUT_TRY_COUNT_MESSAGE("시도할 횟수는 몇 회인가요?");
 
     private final String message;
 
